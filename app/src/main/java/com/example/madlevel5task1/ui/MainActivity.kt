676a752dcee2e51fbfd92a01bb2084bc4413f9f3
joep <joepboekhold@gmail.com>
2020-11-29 18:24:39 +0100

    Finished step 5.2

--- app/src/main/java/com/example/madlevel5task1/ui/MainActivity.kt
+++ app/src/main/java/com/example/madlevel5task1/ui/MainActivity.kt
@@ -4,20 +4,37 @@ import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
 import com.example.madlevel5task1.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        navController = findNavController(R.id.nav_host_fragment)
+
+        fabEditNote.setOnClickListener {
+            navController.navigate(
+                R.id.action_FirstFragment_to_SecondFragment
+            )
+        }
+    }
+
+    private fun fabToggler() {
+        navController.addOnDestinationChangedListener { _,       destination, _ ->
+            if (destination.id in arrayOf(R.id.SecondFragment)) {
+                fabEditNote.hide()
+            } else {
+                fabEditNote.show()
+            }
         }
     }
 
